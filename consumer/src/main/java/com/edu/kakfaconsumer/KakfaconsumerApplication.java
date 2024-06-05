4947e3ee43c62b679092c3285f24b22cab078ee6
Eduardo Diogo <eduardodiogo@MacBook-Pro-de-Eduardo.local>
2024-06-05 20:48:21 -0300

    feat: Initial commit

---
+++ consumer/src/main/java/com/edu/kakfaconsumer/KakfaconsumerApplication.java
@@ -0,0 +1,13 @@
+package com.edu.kakfaconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakfaconsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KakfaconsumerApplication.class, args);
+	}
+
+}
---
+++ consumer/src/test/java/com/edu/kakfaconsumer/KakfaconsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.kakfaconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakfaconsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
